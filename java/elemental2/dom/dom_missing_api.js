a2870b5126c6cd505c5b888c0e89585593eaebde
Thomas Broyer <t.broyer@ltgt.net>
2017-11-26 15:59:13 +0100

    Fix Location.prototype.port type (should be string, not number)

--- java/elemental2/dom/dom_missing_api.js
+++ java/elemental2/dom/dom_missing_api.js
@@ -92,7 +92,7 @@ Location.prototype.pathname;
  * Returns the Location object's URL's port. Can be set, to navigate to the
  * same URL with a changed port.
  * @see https://html.spec.whatwg.org/multipage/history.html#dom-location-port
- * @type {number}
+ * @type {string}
  */
 Location.prototype.port;
 
