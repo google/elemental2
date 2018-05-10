54f1cfce9d90c83dd7de0059689d88391d4ad26f
dramaix <dramaix@google.com>
2018-05-09 17:38:00 -0700

    Make scripts needed to build/release elemental and jsinterop.base available to opensource community.
    PiperOrigin-RevId: 196056277

---
+++ release_elemental.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -i
+#
+# The script generates the open source artifacts for elemental and
+# upload them to sonatype.
+#
+set -e
+
+lib_version=$1
+
+bazel_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+deploy_target='@com_google_jsinterop_generator//:deploy'
+license_header='@com_google_jsinterop_generator//:license.txt'
+group_id="com.google.elemental2"
+
+echo "enter your gpg passphrase:"
+read -s gpg_passphrase
+
+cd ${bazel_root}
+
+elemental_artifacts="core dom indexeddb media promise svg webgl webstorage"
+
+for artifact in ${elemental_artifacts}; do
+  artifact_path=${bazel_root}/bazel-bin/java/elemental2/${artifact}
+  artifact_bazel_path=//java/elemental2/${artifact}
+  jar_file=lib${artifact}.jar
+  src_jar=lib${artifact}-src.jar
+
+  # ask bazel to explicitly build both jar files
+  bazel build ${artifact_bazel_path}:${jar_file}
+  bazel build ${artifact_bazel_path}:${src_jar}
+
+  pom_template=${bazel_root}/maven/pom-${artifact}.xml
+  maven_artifact="elemental2-${artifact}"
+
+  # run that script with bazel as its a dependency of this project
+  bazel run ${deploy_target} -- ${maven_artifact} ${artifact_path}/${jar_file} ${artifact_path}/${src_jar} ${license_header} ${pom_template} ${lib_version} ${group_id} ${gpg_passphrase}
+done
