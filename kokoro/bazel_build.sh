8f403543fc13e7345b2c4fe0c8abbb6ba4fd5362
dramaix <dramaix@google.com>
2018-12-10 16:22:57 -0800

    Use setup_build_environment.sh script for setup kokoro builds for all our opensource repo.
    PiperOrigin-RevId: 224904609

--- kokoro/bazel_build.sh
+++ kokoro/bazel_build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
+chmod +x ${KOKORO_GFILE_DIR}/setup_build_environment.sh
+${KOKORO_GFILE_DIR}/setup_build_environment.sh
+
 # the repo is cloned under git/elemental2
 cd git/elemental2
 
-bazel build ...
+bazel build ... --incompatible_remove_native_http_archive=false
